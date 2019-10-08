977ef3026ac094a2dfba9ad866e52ccf85d2ddd2
unknown <ghdwlgus0607@naver.com>
2019-10-08 11:58:14 +0900

    정적 컨텐츠

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+#from . import views
 
-urlpatterns = [    path('', views.homepage, name='home'),
+urlpatterns = [
+    #path('', views.homepage, name='home'),
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
