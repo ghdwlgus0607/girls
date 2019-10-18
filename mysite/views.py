1543e3754992ce423359fec2ba0a232cd4319f28
unknown <ghdwlgus0607@naver.com>
2019-10-18 19:18:47 +0900

    Modified templates to display posts from database.

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+ #views.postlist 함수는 db에서 필요한 데이터를 가져와서
+ #postlist.html에게 넘겨줘야 함.
 def homepage(request):
     return HttpResponse('여기는 홈 페이지')
